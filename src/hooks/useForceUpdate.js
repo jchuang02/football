c3f2e86a96341f193dbe29b585c0e0cd4b2a12f7
jchuang02 <jchuangsa@gmail.com>
2022-03-12 21:06:07 -0800

    Remove unused plugin and hook

--- src/hooks/useForceUpdate.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function useForceUpdate() {
-  const [value, setValue] = useState(0);
-  console.log(value);
-  return () => setValue((value) => value + 1);
-}
